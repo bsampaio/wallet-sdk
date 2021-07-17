c84fa247a1724fb601ff409c9a5e08ffb8e79367
Paulo Olivetti <paulo.junior@globalsys.com>
2021-07-17 12:43:02 -0300

    accountType in models

--- src/Domains/DigitalAccount.php
+++ src/Domains/DigitalAccount.php
@@ -13,7 +13,8 @@ abstract class DigitalAccount extends Model
 {
     const COMPANY_TYPES = ["MEI", "EI", "EIRELI", "SA", "LTDA", "INSTITUITION_NGO_ASSOCIATION"];
 
-    protected $type = "PAYMENT";
+    protected $type = "PJ";
+    protected $accountType = "PAYMENT";
     protected $name, $document, $email, $phone;
     protected $businessArea, $linesOfBusiness;
     protected $address;
@@ -39,10 +40,11 @@ abstract class DigitalAccount extends Model
      * @param bool $emailOptOut
      * @param bool $autoTransfer
      */
-    public function __construct(string $type, string $name, string $document, string $email, string $phone, string $businessArea, string $linesOfBusiness, Address $address, BankAccount $bankAccount, float $monthlyIncomeOrRevenue, $pep = false, bool $emailOptOut = false, bool $autoTransfer = false)
+    public function __construct(string $type, $accountType, string $name, string $document, string $email, string $phone, string $businessArea, string $linesOfBusiness, Address $address, BankAccount $bankAccount, float $monthlyIncomeOrRevenue, $pep = false, bool $emailOptOut = false, bool $autoTransfer = false)
     {
         $this->type = $type;
         $this->name = $name;
+        $this->$accountType;
         $this->document = $document;
         $this->email = $email;
         $this->phone = $phone;
